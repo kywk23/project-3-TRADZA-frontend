4c318feceb1726ae65379105a094833973d3a025
kywk23 <kelvinywk.work@outlook.com>
2024-01-17 16:46:53 +0800

    base clean up

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,9 @@ function App() {
 
   return (
     <>
-      <div></div>
-      <h1>BENJAMIN + KELVIN 'S PROJECT 3</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h1>BENJAMIN + KELVIN 'S PROJECT 3</h1>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
